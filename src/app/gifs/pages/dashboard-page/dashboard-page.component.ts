74503c3e42892fcd3a31c677094e2bbf883b0368
Carlos Verdugo <carlos.versa99@gmail.com>
2025-06-17 21:53:25 -0700

    Adding tailwind and lazy routes

---
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [],
+  templateUrl: './dashboard-page.component.html',
+  styleUrl: './dashboard-page.component.css'
+})
+export default class DashboardPageComponent {
+
+}
