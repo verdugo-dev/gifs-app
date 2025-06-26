3103ca5c9fa46610d23ccacafee5a614355c2076
Carlos Verdugo <carlos.versa99@gmail.com>
2025-06-25 22:42:35 -0700

    Adding side options

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,17 +1,38 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 interface MenuOption {
   label: string;
   subLabel: string;
-  router: string;
+  route: string;
   icon: string;
 }
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './side-menu-options.component.html',
   styleUrl: './side-menu-options.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-search',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ]
+
+ }
