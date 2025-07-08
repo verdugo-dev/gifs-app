e412e3432bda483eb32e429c8c15b354987b18d5
Carlos Verdugo <carlos.versa99@gmail.com>
2025-07-07 22:18:21 -0700

    Adding search gif method on service

--- src/app/gifs/pages/search-page/search-page.component.ts
+++ src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { GifService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.css'
 })
 export default class SearchPageComponent {
+
+  gifService = inject( GifService );
+
+  onSearch(query: string) {
+    console.log(query)
+    this.gifService.searchGifs(query);
+  }
 
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -32,4 +32,18 @@ export class GifService {
 
   }
 
+  searchGifs(query: string) {
+    this.http.get<GiphyResponse>(`${environment.giphyURL}/gifs/search`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        limit: 20,
+        q: query,
+      },
+    })
+    .subscribe((resp) => {
+      const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+      console.log({search: gifs})
+    });
+  }
+
 }
